27e20f071f8f6595d06ccffd8d3bc472e8df308b
tftprodigy <Olurombisammy@gmail.com>
2024-06-20 00:56:21 +0100

    02.Django template Language (rendering html files)

--- myProject/firstDjangoApp/views.py
+++ myProject/firstDjangoApp/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hey, Welcome To Homepage.</h1>')
+    # return HttpResponse('<h1>Hey, Welcome To Homepage.</h1>')
+   return render(request, 'index.html')
